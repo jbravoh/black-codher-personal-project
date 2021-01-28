57fb9c5a58678c6aeb1fa92e3e9581c8730ce530
jbravoh <68926455+jbravoh@users.noreply.github.com>
2021-01-28 15:55:37 +0000

    Token is fixed

--- server/models/clients.js
+++ server/models/clients.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const bcrypt = require("bcrypt");
 
 const clientSchema = new Schema({
   client_id: Number,
@@ -34,17 +33,21 @@ clientSchema.pre("save", function (next) {
   // Check to see if document is new or a new password has been set
   if (this.isNew || this.isModified("password")) {
     // Saving reference to this because of changing scopes
-    const document = this;
-    bcrypt.hash(document.password, saltRounds, function (err, hashedPassword) {
-      if (err) {
-        next(err);
-      } else {
-        document.password = hashedPassword;
-      }
-    });
   } else {
     next();
   }
 });
+//     const document = this;
+//     bcrypt.hash(document.password, saltRounds, function (err, hashedPassword) {
+//       if (err) {
+//         next(err);
+//       } else {
+//         document.password = hashedPassword;
+//       }
+//     });
+//   } else {
+//     next();
+//   }
+// });
 
 module.exports = mongoose.model("clients", clientSchema);
